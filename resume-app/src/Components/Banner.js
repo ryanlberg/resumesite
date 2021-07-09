6e9fd50f64a561b6c368a623bd8162830c836d93
Ryan Berg <ryanmcberg@gmail.com>
2021-07-09 16:30:36 -0400

    Added sm icons to banner

--- resume-app/src/Components/Banner.js
+++ resume-app/src/Components/Banner.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-
+import {SocialIcon} from 'react-social-icons';
 class Banner extends Component {
     render(){
         return(
             <div className="Banner">
-
+                <div className="Social">
+                    <span>
+                    <SocialIcon url="https://www.linkedin.com/in/ryan-berg-94824856/"/> <a href="https://www.linkedin.com/in/ryan-berg-94824856/">Linkedin</a>                
+                    </span>
+                    <span>
+                    <SocialIcon url="https://github.com/ryanlberg" /> <a href="https://github.com/ryanlberg">Github</a>
+                    </span>
+                    <span className="Name">
+                        Ryan Berg
+                    </span>
+                </div>
+                
             </div>
         )
     
